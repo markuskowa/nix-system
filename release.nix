51e8cb87f4d2f49a5c95cb08a88de6816cd457f5
Markus Kowalewski <markus.kowalewski@gmail.com>
2020-06-16 22:28:43 +0200

    release.nix: add tests and cleanup

--- release.nix
+++ release.nix
@@ -1,18 +1,25 @@
-{ nixpkgs ? (import <nixpkgs>) { overlays = [ (import ./default.nix) ]; } } :
+{ nixpkgs ? <nixpkgs>
+, system ? builtins.currentSystem
+} :
 
 let
   handleTest = t: (import <nixpkgs/nixos/tests/make-test.nix>) (import t);
+  pkgs = (import nixpkgs) { overlays = [ (import ./default.nix) ]; };
 
-in {
+  #nixosTests = import "${nixpkgs}/nixos/tests/all-tests.nix" {
+  #  inherit pkgs system;
+  #  callTest = t: pkgs.lib.hydraJob t.test;
+  #};
+
+in rec {
   # Evaluate overlay packages
-  inherit (nixpkgs)
+  inherit (pkgs)
     nfs-ganesha
-    orangefs
     ipdeny-zones
     redfishtool;
 
   # Tests
-  tests = nixpkgs.recurseIntoAttrs {
+  tests = {
     infiniband = handleTest ./tests/infiniband.nix {};
     nfs-ganesha = handleTest ./tests/nfs-ganesha.nix {};
     moosefs = handleTest ./tests/moosefs.nix {};
@@ -20,5 +27,35 @@ in {
     networkmap = handleTest ./tests/networkmap.nix {};
     sshCA = handleTest ./tests/sshCA.nix {};
     zfsAttrs = handleTest ./tests/zfs-attr.nix {};
+  };
+
+  upstreamTests = {
+    inherit (pkgs.nixosTests)
+      borgbackup
+      influxdb
+      grafana
+      redmine
+      slurm
+      telegraf;
+  };
+
+  tested = pkgs.releaseTools.aggregate {
+    name = "tests";
+    constituents = [
+      "tests.nfs-ganesha"
+      "tests.moosefs"
+      "tests.banner"
+      "tests.sshCA"
+      "tests.networkmap"
+      "tests.zfsAttrs"
+      # essential upstream tests
+      "upstreamTests.borgbackup"
+      "upstreamTests.influxdb"
+      "upstreamTests.grafana"
+      "upstreamTests.redmine.mysql"
+      "upstreamTests.redmine.pgsql"
+      "upstreamTests.slurm"
+      "upstreamTests.telegraf"
+    ];
   };
 }
