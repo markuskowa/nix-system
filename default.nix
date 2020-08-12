576373ed36ab87f86831ae9b1bf0240c144fb721
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2020-08-12 12:50:28 +0200

    nfs-ganesha: remove, it is included in nixkgs master now

--- default.nix
+++ default.nix
@@ -4,10 +4,6 @@ let
   callPackage = super.callPackage;
 
 in {
-  nfs-ganesha = callPackage ./pkgs/nfs-ganesha {};
-
-  ntirpc = callPackage ./pkgs/ntirpc {};
-
   ipdeny-zones = callPackage ./pkgs/ipdeny-zones {};
 
   redfishtool = callPackage ./pkgs/redfishtool {};
