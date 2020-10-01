913d3488166bcf5eb1b813c99faec7924893d433
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2020-10-01 14:50:04 +0200

    ipdeny-zones: 20200621 -> 20201001

--- pkgs/ipdeny-zones/default.nix
+++ pkgs/ipdeny-zones/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "ipdeny";
-  version = "20200621";
+  version = "20201001";
 
   src = fetchFromGitHub {
     owner = "markuskowa";
     repo = "ipdeny-zones";
     rev = version;
-    sha256 = "0pd0glanazij12zkbn3z46kr6yzk2kbifck2ys4y2h6r8vcxl3fq";
+    sha256 = "06lndsfnq66gi9dfz42ry7hz4zx48gijvxcdy149q3nxmak2vzn2";
   };
 
   installPhase = ''
