c02eae49d41dce0189f28047f3555e4d3d00afef
Markus Kowalewski <markus.kowalewski@gmail.com>
2021-12-19 20:44:11 +0100

    nixos/geoblocking: fix test

--- tests/geoblocking.nix
+++ tests/geoblocking.nix
@@ -17,7 +17,7 @@
       };
 
       networking.firewall = {
-        enable = false;
+        enable = true;
         countries = [ "us" ];
         countryWhitelist = true;
       };
@@ -36,9 +36,7 @@
         eth3.ipv4.addresses = lib.mkOverride 0 [ { address = "5.0.0.1"; prefixLength = 24; } ];
       };
 
-      networking.firewall = {
-        enable = false;
-      };
+      networking.firewall.enable = false;
 
       boot.kernel.sysctl = {
         "net.ipv4.conf.all.forwarding" = true;
@@ -47,30 +45,30 @@
     };
 
     client1 = { config, lib, ... } :  {
-      virtualisation.vlans = [ 3 ];
+      virtualisation.vlans = [ 2 ];
       imports = [ ../modules/overlay.nix ];
       networking.interfaces = {
-        eth3.ipv4.addresses = lib.mkOverride 0 [ { address = "5.0.0.2"; prefixLength = 24; } ];
+        eth1.ipv4.addresses = lib.mkOverride 0 [ { address = "4.0.0.2"; prefixLength = 24; } ];
       };
 
       networking.defaultGateway = {
-        address = "5.0.0.1";
-        interface = "eth3";
+        address = "4.0.0.1";
+        interface = "eth1";
       };
 
       networking.firewall.enable = false;
     };
 
     client2 = { config, lib, ... } :  {
-      virtualisation.vlans = [ 2 ];
+      virtualisation.vlans = [ 3 ];
       imports = [ ../modules/overlay.nix ];
       networking.interfaces = {
-        eth2.ipv4.addresses = lib.mkOverride 0 [ { address = "4.0.0.2"; prefixLength = 24; } ];
+        eth1.ipv4.addresses = lib.mkOverride 0 [ { address = "5.0.0.2"; prefixLength = 24; } ];
       };
 
       networking.defaultGateway = {
-        address = "4.0.0.1";
-        interface = "eth2";
+        address = "5.0.0.1";
+        interface = "eth1";
       };
 
       networking.firewall.enable = false;
@@ -80,13 +78,10 @@
   testScript = ''
     router.start()
     server.wait_for_unit("network.target")
-
     client1.wait_for_unit("network.target")
-    client1.succeed("ip a 1>&2")
-    client1.succeed("ip r 1>&2")
-    client1.succeed("ping -c 1 server")
+    client1.succeed("ping -c 1 10.10.0.2")
 
     client2.wait_for_unit("network.target")
-    client2.succeed("ping -c 1 server")
+    client2.fail("ping -c 1 10.10.0.2")
   '';
 }
