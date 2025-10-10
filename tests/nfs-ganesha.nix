2722fb26dd6c52460c5e34d2df50bbc9812a9450
Markus Kowalewski <markus.kowalewski@gmail.com>
2025-10-10 23:17:40 +0200

    tests/nfs-ganesha: add tests for XFS and BTRFS

--- tests/nfs-ganesha.nix
+++ tests/nfs-ganesha.nix
@@ -6,14 +6,24 @@ let
     networking.firewall.allowedTCPPorts = [ 2049 ];
     boot.initrd.postDeviceCommands = ''
       ${pkgs.e2fsprogs}/bin/mkfs.ext4 -L data /dev/vdb
+      ${pkgs.xfsprogs}/bin/mkfs.xfs -L data_xfs /dev/vdc
+      ${pkgs.btrfs-progs}/bin/mkfs.btrfs -L data_btrfs /dev/vdd
     '';
 
-    virtualisation.emptyDiskImages = [ 4096 ];
+    virtualisation.emptyDiskImages = [ 1024 1024 1024 ];
 
     fileSystems = pkgs.lib.mkVMOverride {
-      "/data" = {
+      "/data_vfs" = {
         device = "/dev/disk/by-label/data";
         fsType = "ext4";
+      };
+      "/data_xfs" = {
+        device = "/dev/disk/by-label/data_xfs";
+        fsType = "xfs";
+      };
+      "/data_btrfs" = {
+        device = "/dev/disk/by-label/data_btrfs";
+        fsType = "btrfs";
       };
     };
 
@@ -34,9 +44,37 @@ let
       };
       exports = [
         {
-          Export_Id = 0;
-          Path = "/data";
-          Pseudo = "/";
+          Export_Id = 1;
+          Path = "/data_vfs";
+          Pseudo = "/vfs";
+          Squash = "None";
+
+          Protocols = "4";
+
+          Access_Type = "RW";
+
+          FSAL = {
+            Name = "VFS";
+          };
+        }
+        {
+          Export_Id = 2;
+          Path = "/data_xfs";
+          Pseudo = "/xfs";
+          Squash = "None";
+
+          Protocols = "4";
+
+          Access_Type = "RW";
+
+          FSAL = {
+            Name = "XFS";
+          };
+        }
+        {
+          Export_Id = 3;
+          Path = "/data_btrfs";
+          Pseudo = "/btrfs";
           Squash = "None";
 
           Protocols = "4";
@@ -83,8 +121,9 @@ in {
       client.wait_for_unit("data.mount")
 
     # R/W test between clients
-    client1.succeed("echo test > /data/file1")
-    client1.wait_until_succeeds("grep test /data/file1")
-    client2.wait_until_succeeds("grep test /data/file1")
+    for fs in ["vfs", "xfs", "btrfs"]:
+      client1.succeed(f"echo test > /data/{fs}/file1")
+      client1.wait_until_succeeds(f"grep test /data/{fs}/file1")
+      client2.wait_until_succeeds(f"grep test /data/{fs}/file1")
   '';
 }
