f2bb7232c6554818c12ce36df43a2292b907f43c
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2025-10-24 13:31:44 +0200

    nixos/nfs-ref: fix test

--- tests/nfs-ref.nix
+++ tests/nfs-ref.nix
@@ -1,16 +1,29 @@
 { pkgs, ... } :
+let
+  exportsDir =  "/data/ref_exports";
+  nfsrefDir =  "/data/ref_nfsref";
 
-{
-  name = "nfs-ref";
+in {
+  name = "nfs-referrals";
 
   nodes = {
     server1 = {...} : {
       services.nfs.server = {
         enable = true;
         exports = ''
-          /data 192.168.1.0/255.255.255.0(rw,no_root_squash,no_subtree_check,fsid=0)
+          /data 192.168.1.0/24(rw,no_root_squash,no_subtree_check,fsid=0)
+          ${exportsDir} 192.168.1.0/24(rw,no_root_squash,no_subtree_check,refer=/@server2)
         '';
         createMountPoints = true;
+      };
+
+      # the exports referral needs a dummy bind mount
+      virtualisation.fileSystems  = {
+        "${exportsDir}" = {
+          device = "${exportsDir}";
+          fsType = "none";
+          options = [ "bind" ];
+        };
       };
 
       networking.firewall.enable = false;
@@ -20,7 +33,7 @@
       services.nfs.server = {
         enable = true;
         exports = ''
-          /data 192.168.1.0/255.255.255.0(rw,no_root_squash,no_subtree_check,fsid=0)
+          /data 192.168.1.0/24(rw,no_root_squash,no_subtree_check,fsid=0)
         '';
         createMountPoints = true;
       };
@@ -28,32 +41,47 @@
       networking.firewall.enable = false;
     };
 
-    client = {...} : {
+    client = {pkgs,lib,...} : {
 
       virtualisation.fileSystems  = {
         "/data" = {
           device = "server1:/";
-          fsType = "nfs";
+          fsType = "nfs4";
           options = [ "vers=4.2" ];
         };
       };
+
+      environment.etc."request-key.conf".text = lib.mkForce ''
+        create dns_resolver  * * ${pkgs.keyutils}/bin/key.dns_resolver %k
+        create id_resolver   * * ${pkgs.nfs-utils}/bin/nfsidmap -t 600 %k %d
+      '';
 
       networking.firewall.enable = false;
     };
   };
 
   testScript = ''
+    server2.start();
     server1.wait_for_unit("nfs-server.service");
+    server1.succeed(
+        "mkdir ${nfsrefDir}",
+        "nfsref add ${nfsrefDir} server2 /"
+        );
+
     server2.wait_for_unit("nfs-server.service");
-    server1.succeed("mkdir /data/ref && nfsref add /data/ref server2 /");
     client.wait_for_unit("data.mount");
 
-    client.succeed("echo hallo > /data/foo");
-    server1.succeed("grep hallo /data/foo");
+    with subtest("Basic NFS functionality"):
+      client.succeed("echo hallo > /data/foo");
+      server1.succeed("grep hallo /data/foo");
 
-    client.succeed("ls -ld /data/ref/");
-    client.succeed("echo hallo > /data/ref/foo");
-    server2.succeed("grep hallo /data/ref/foo");
+    with subtest("referral via nfsref"):
+      client.wait_until_succeeds("touch ${nfsrefDir}/nfsref");
+      server2.wait_for_file("/data/nfsref");
+
+    with subtest("referral via exports"):
+      client.succeed("touch ${exportsDir}/exports");
+      server2.wait_for_file("/data/exports");
   '';
 }
 
